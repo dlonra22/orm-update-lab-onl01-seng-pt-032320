a549a69921ba10de9eb4cbc21d4009f7b67ce589
arnie b <arnold.bwaila@gmail.com>
2020-06-11 16:42:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,6 +51,16 @@ def self.new_from_db(row)
   student.grade = row[2]
   student
 end
+
+def self.find_by_name(name)
+  sql = <<-SQL 
+        SELECT * FROM students
+        WHERE students.name = ?
+        LIMIT 1
+        SQL
+  DB[:conn].execute(sql,name).collect do |row|
+    self.new_from_db(row)
+  end
   
   
         
